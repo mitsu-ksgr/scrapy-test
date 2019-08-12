466ecd15b21347f2c7d421e2e2496fffef6695bd
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2019-08-12 23:06:25 +0900

    support 'startporject' command

--- run.sh
+++ run.sh
@@ -3,5 +3,20 @@
 readonly DOCKER_IMG='python-scrapy-test:latest'
 
 docker build -t ${DOCKER_IMG} .
-docker run --rm -t ${DOCKER_IMG}
+
+# ./run.sh st ProjectName
+if [ $# -eq 2 ]; then
+    if [ "$1" = 'st' ]; then
+        docker run --rm \
+            -v "$(pwd)/src":/output \
+            -t ${DOCKER_IMG} \
+            -s "$2"
+
+        sudo chown -R $USER:$USER ./src/$2
+        exit 0
+    fi
+fi
+
+# ./run.sh
+docker run --rm -t ${DOCKER_IMG} $@
 
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -11,7 +11,52 @@
 
 #python /src/test.py $@
 
-scrapy runspider /src/test.py
+#
+# Flags
+#
+OPT_FLAG_START_PROJECT=
 
 
+#
+# Parse args
+#
+parse_args() {
+    while getopts s flag; do
+        case "${flag}" in
+            s ) # Start Project
+                OPT_FLAG_START_PROJECT='true'
+                ;;
+            * )
+                exit 0
+                ;;
+        esac
+    done
+}
+
+err() {
+    echo "Error: $@" 1>&2
+    exit 1
+}
+
+main() {
+    parse_args $@
+    shift $(expr $OPTIND - 1)
+
+    if [ -n "${OPT_FLAG_START_PROJECT}" ]; then
+        local project_name=$1
+        if [ -z "${project_name}" ]; then
+            err "please specify a project name"
+        fi
+
+        local project_path="/output/${project_name}"
+        scrapy startproject ${project_name} ${project_path}
+        exit 0
+    fi
+
+    scrapy runspider /src/test.py
+    exit 0
+}
+
+main $@
 exit 0
+
