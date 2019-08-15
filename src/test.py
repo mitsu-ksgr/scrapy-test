df7051fbb91fb8c2264d6fe93f6540168c48635a
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2019-08-15 21:20:16 +0900

    add custom settings

--- src/test.py
+++ src/test.py
@@ -5,6 +5,9 @@ import scrapy
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
     start_urls = ['https://blog.scrapinghub.com/']
+    custom_settings = {
+        'DOWNLOAD_DELAY': 10.0, # sec
+    }
 
     def parse(self, response):
         for title in response.css('.post-header>h2'):
